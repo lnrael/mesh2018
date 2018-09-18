bf87f7ebee7ca1943d213c535544450636288de0
tyler\allie.guo <allie.guo@tylertech.com>
2018-09-18 08:10:57 -0400

    step 1 new component used in a router outlet app takes up full space of screen use a prebuilt material theme

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,24 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Route } from '@angular/router';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { ContactViewComponent } from './contact-view/contact-view.component';
 
 const routes: Route[] = [
-  // { path: '', redirectTo: '', pathMatch: 'full' },
-  // { path: '', component: },
+  { path: '', redirectTo: 'Contacts/Search', pathMatch: 'full' },
+  { path: 'Contacts/Search', component: ContactViewComponent }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ContactViewComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }), // Includes all basic angular directives
     HttpClientModule, // Includes angular http providers
     FormsModule, // Form/input directives
     RouterModule.forRoot(routes), // Register routes,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
